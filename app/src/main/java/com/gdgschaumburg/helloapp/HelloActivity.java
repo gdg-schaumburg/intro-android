8fe10ca2cf709995a139e8069df717f32bc7b437
Ryan Harter <ryanjharter@gmail.com>
2014-08-11 08:39:50 -0500

    Adds dynamic text to hello activity label.

--- app/src/main/java/com/gdgschaumburg/helloapp/HelloActivity.java
+++ app/src/main/java/com/gdgschaumburg/helloapp/HelloActivity.java
@@ -2,14 +2,20 @@ package com.gdgschaumburg.helloapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by rharter on 8/11/14.
  */
 public class HelloActivity extends Activity {
 
+    private TextView mHelloText;
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+
+        mHelloText = (TextView) findViewById(R.id.hello_text);
+        mHelloText.setText("Hello Code");
     }
 }

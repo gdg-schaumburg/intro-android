f6ebdf7714a0774419c27af56abe3d4985915bdf
Ryan Harter <ryanjharter@gmail.com>
2014-08-12 16:09:03 -0500

    Adds EditText to get user's name and change text accordingly.

--- app/src/main/java/com/gdgschaumburg/helloapp/HelloActivity.java
+++ app/src/main/java/com/gdgschaumburg/helloapp/HelloActivity.java
@@ -2,6 +2,10 @@ package com.gdgschaumburg.helloapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -10,12 +14,22 @@ import android.widget.TextView;
 public class HelloActivity extends Activity {
 
     private TextView mHelloText;
+    private EditText mNameInput;
+    private Button mSubmitButton;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
 
         mHelloText = (TextView) findViewById(R.id.hello_text);
-        mHelloText.setText("Hello Code");
+        mNameInput = (EditText) findViewById(R.id.name_input);
+
+        mSubmitButton = (Button) findViewById(R.id.submit_button);
+        mSubmitButton.setOnClickListener(new OnClickListener() {
+            @Override public void onClick(View v) {
+                String name = mNameInput.getText().toString();
+                mHelloText.setText("Hello, " + name);
+            }
+        });
     }
 }
